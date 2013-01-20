f39d58061a31b4ee1063ca233c0f4ca28eca153c
Cameron Dutro <camertron@gmail.com>
2013-01-20 01:00:08 -0800

    Extra version information

--- lib/jvectormap-rails.rb
+++ lib/jvectormap-rails.rb
@@ -1,3 +1,4 @@
 # encoding: UTF-8
 
 require 'jvectormap/rails/engine'
+require 'jvectormap/rails/version'
--- lib/jvectormap/rails/version.rb
+++ lib/jvectormap/rails/version.rb
@@ -3,5 +3,6 @@
 module JVectorMap
   module Rails
     VERSION = "1.0.0"
+    JVECTORMAP_VERSION = "1.2.1"
   end
 end
